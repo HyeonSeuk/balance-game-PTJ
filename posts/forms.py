883fbf2cf0ebb7ea138d5939f6e935949ffe37bf
HyeonSeuk <seo5719@naver.com>
2023-04-20 10:28:02 +0900

    topic-c

--- posts/forms.py
+++ posts/forms.py
@@ -5,8 +5,7 @@ from .models import Post, Comment
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('title','content',)
-
+        fields = '__all__'
 
 class CommentForm(forms.ModelForm):
     class Meta:
